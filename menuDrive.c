c948f56511e7f1444346658feb57254d8dafbcc6
bhanu79755 <bhanushreeka2004@gmail.com>
2024-01-05 20:07:34 +0530

    to check palindrome and display the menu drive

--- menuDrive.c
+++ menuDrive.c
@@ -12,35 +12,36 @@ void main()
     while(1)
 {
 printf("\n**********************************MENU************************\n");
-printf("\n 1:push 2:pop 3:palindrome work 4:status of stack 5:display 6:exit");
-printf("enter your choice:");
+printf("\n 1:Push\n\t2:Pop\n\t3:Palindrome Work\n\t4:Status of stack\n\t5:Display\n\t6:exit\n\n");
+printf("Enter your choice:");
 scanf("%d",&choice);
 switch(choice)
 {
-    case 1:printf("enter the item to be inserted:");
+    case 1:printf("Enter the item to be inserted:");
            scanf("%d",&item);
            push(item);
            break;
     case 2:element=pop();
            if(element)
-            printf("the item deleted is%d",element);
+            printf("The item deleted is%d",element);
             break;
-    case 3:printf("demonstration of  how stack can be used to check palindrom\n");
+    case 3:printf("Demonstration of  how stack can be used to check palindrome\n");
            if(top==-1)
             palindrome();
            else
-           printf("make first stack empty\n");
+           printf("Make first stack empty\n");
            break;
     case 4:printf("**********status of stack****************") ;
-           printf("tptal number of elements can be inserted into stack:%d\n",STACKSIZE);
-           printf("the total number of elemnets present in stack:%d\n,top+1");
-           printf("number of loaction free in stack:%d",(STACKSIZE-top-1));
+           printf("T0tal number of elements can be inserted into stack:%d\n",STACKSIZE);
+           printf("The total number of elemnets present in stack:%d\n,top+1");
+           printf("Number of loaction free in stack:%d",(STACKSIZE-top-1));
            break;
     case 5:display();
            break;
     case 6:exit(0);
-    default:printf("unknow choice\n");
+    default:printf("invalid choice\n");
             break;
+            
 }
 }
 }
@@ -48,7 +49,7 @@ void push(int item)
 {
     if(top==STACKSIZE-1)
     {
-        printf("stack over flow\n");
+        printf("Stack over flow\n");
         return;
     }
     stack[++top]=item;
@@ -57,14 +58,51 @@ int pop()
 {
     if(top==-1)
     {
-        printf("stack underflow\n");
+        printf("Stack underflow\n");
         return(0);
     }
     return(stack[top--]);
 }
+void palindrome()
+{
+    int digit,rev=0,lenth,m,num,a[5],n=0,i;
+    printf("Enter the element:");
+    scanf("%d",&num);
+    m=num;
+    while(m!=0)
+    {
+        digit=m%10;
+        n++;
+        a[n]=digit;
+        m=m/10;
+    }
+    for(i=n;i>0;i--)
+        push(a[i]);
 
+    while(top!=-1)
+    {
+        digit=pop();
+        rev=rev*10+digit;
+    }
+    if(num==rev)
+    printf("\n The given number is palindrome\n");
+    else 
+    printf("\nThe given  umber is not palindrome\n");
+ }
 
+    void display()
+    {
+        int i;
+        if(top==-1)
+        {
+            printf("Stack is Empty\n");
+            return;
+        }
 
-
-
-}
+            printf("The contents of Stack are\n");
+            for(i=top;i>=0;i--)
+            {
+                printf("%d\n",stack[i]);
+            }
+        
+    }
